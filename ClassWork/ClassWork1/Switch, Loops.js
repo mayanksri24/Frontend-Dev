5801ef7aac3a9d47c727e9ce335c70edb15139ce
Mayank Shree <mayank.shree24@gmail.com>
2025-11-17 13:22:21 +0530

    Create Switch, Loops.js

---
+++ ClassWork/ClassWork1/Switch, Loops.js
@@ -0,0 +1,21 @@
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("Enter day no. (1 to 7): ", function(day) {
+    day = parseInt(day);
+
+    switch(day) {
+        case 1: console.log("Monday"); break;
+        case 2: console.log("Tuesday"); break;
+        case 3: console.log("Wednesday"); break;
+        case 4: console.log("Thursday"); break;
+        case 5: console.log("Friday"); break;
+        case 6: console.log("Saturday"); break;
+        case 7: console.log("Sunday"); break;
+        default: console.log("Invalid day");
+    }
+
+    readline.close();
+});
